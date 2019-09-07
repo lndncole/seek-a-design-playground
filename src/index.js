a4a0fc5c155a61a98c2fb794020623417c4840f3
Landon <lndncole@gmail.com>
2019-09-07 13:30:51 -0500

    re-removed index.js files for the sake of switching branches

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
-// $(document).ready(function(){
+$(document).ready(function(){
 
-//   // jQuery goes here...
-//   $('.center').fadeToggle(1500);
-
-//   setInterval(function() {
-//     $('.center').fadeToggle(1500);
-//   }, 1500);
-
-// });
+});
 
 
