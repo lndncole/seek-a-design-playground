a9a1f6043810918bdfd744b1ea29286adb46c75e
Landon <lndncole@gmail.com>
2019-09-10 14:17:55 -0400

    reverting the last commit Revert "attempted to some work on adding the menu hover color change functionality"
    This reverts commit ca88e569218da2d1cfb17cf90608f7abc284e3e0.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 $(document).ready(function(){
 //selecting html elements for use in color change functions
+document.getElementById("name").onkeydown = function() {nameInputBackgroundChange()};
+document.getElementById("mail").onkeydown = function() {mailInputBackgroundChange()};
+document.getElementById("subject").onkeydown = function() {subjectInputBackgroundChange()};
+document.getElementById("msg").onkeydown = function() {msgInputBackgroundChange()};
+
+//selecting html elements for use in reverting colors back to original color upon focus change
+document.getElementById("name").onchange = function() {nameInputChange()};
+document.getElementById("mail").onchange = function() {mailInputChange()};
+document.getElementById("subject").onchange = function() {subjectInputChange()};
+document.getElementById("msg").onchange = function() {msgInputChange()};
 
 // functions to change input/text area color and background color based on userinput
-document.getElementById("name").onkeydown = function() {nameInputBackgroundChange()};
 function nameInputBackgroundChange() {
   var nameField = document.getElementById("name");
   nameField.style.backgroundColor = "white";
   nameField.style.color = "#4D766E";
 }
 
-document.getElementById("name").onchange = function() {nameInputChange()};
 function nameInputChange() {
   var nameField = document.getElementById("name");
   if (nameField.value === '') {
@@ -18,14 +26,12 @@ function nameInputChange() {
   }
 }
 
-document.getElementById("mail").onkeydown = function() {mailInputBackgroundChange()};
 function mailInputBackgroundChange() {
   var mailField = document.getElementById("mail");
   mailField.style.backgroundColor = "white";
   mailField.style.color = "#4D766E";
 }
 
-document.getElementById("mail").onchange = function() {mailInputChange()};
 function mailInputChange() {
   var mailField = document.getElementById("mail");
   if (mailField.value === '') {
@@ -34,14 +40,12 @@ function mailInputChange() {
   }
 }
 
-document.getElementById("subject").onkeydown = function() {subjectInputBackgroundChange()};
 function subjectInputBackgroundChange() {
   var subjectField = document.getElementById("subject");
   subjectField.style.backgroundColor = "white";
   subjectField.style.color = "#4D766E";
 }
 
-document.getElementById("subject").onchange = function() {subjectInputChange()};
 function subjectInputChange() {
   var subjectField = document.getElementById("subject");
   if (subjectField.value === '') {
@@ -49,14 +53,13 @@ function subjectInputChange() {
     subjectField.style.color = "white";
   }
 }
-document.getElementById("msg").onkeydown = function() {msgInputBackgroundChange()};
+
 function msgInputBackgroundChange() {
   var msgField = document.getElementById("msg");
   msgField.style.backgroundColor = "white";
   msgField.style.color = "#4D766E";
 }
 
-document.getElementById("msg").onchange = function() {msgInputChange()};
 function msgInputChange() {
   var msgField = document.getElementById("msg");
   if (msgField.value === '') {
@@ -67,39 +70,28 @@ function msgInputChange() {
 
 // function to display input values in window alert or alert user of needed inputs
 document.getElementById("contact-send").onclick = function() {contactValueAlert()};
+
 function contactValueAlert(){
   var nameField = document.getElementById("name");
   var mailField = document.getElementById("mail");
   var subjectField = document.getElementById("subject");
   var msgField = document.getElementById("msg");
 
-  if(nameField.value === '' || mailField.value === '' || subjectField.value ===
-  '' || msgField.value === '') {
+  if(nameField.value === '' || mailField.value === '' || subjectField.value === '' || msgField.value === '') {
     alert("All required fields of the contact form have not been completed.")
   } else {
-    alert(`${nameField.value} \n ${mailField.value} \n ${subjectField.value} \n
-    ${msgField.value}`);
+    alert(`${nameField.value} \n ${mailField.value} \n ${subjectField.value} \n ${msgField.value}`);
   }
 
 }
 
-//menu hover functipnality
-//unable to get the classlist to toggle. When I click it activates, and i see
-// the class list light up for the target ID eleme - but no actual toggle of class
-document.getElementById("left-center-button").addEventListener("click", navPopOut);
-function navPopOut(){
-  console.log("hello");
-  document.getElementById("myDropdown").classList.toggle("show");
-}
+//   // jQuery goes here...
+//   $('.center').fadeToggle(1500);
 
-window.onclick = function(e) {
-  if (!e.target.matches('#myDropdown')) {
-  var myDropdown = document.getElementById("myDropdown");
-    if (myDropdown.classList.contains('show')) {
-      myDropdown.classList.remove('show');
-    }
-  }
-}
-
+//   setInterval(function() {
+//     $('.center').fadeToggle(1500);
+//   }, 1500);
 
 });
+
+
